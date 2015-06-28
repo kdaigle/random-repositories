8e9535a6e53b1f4f1b558cfa0eaf67c8ed20a509
Kyle Daigle <kyle.daigle@github.com>
2015-06-27 22:56:25 -0400

    Add a sample data file and a comment

--- random_repos.rb
+++ random_repos.rb
@@ -5,7 +5,7 @@ require "octokit"
 require "csv"
 
 client = Octokit::Client.new(:access_token => ENV["ACCESS_TOKEN"])
-path_to_bigquery_csv = "REPLACE THIS"
+path_to_bigquery_csv = "./data.csv"
 
 def repo_name_from_row(row)
   row[0].gsub("https://github.com/", "")
@@ -14,5 +14,6 @@ end
 CSV.foreach(path_to_bigquery_csv) do |row|
   repo_name = repo_name_from_row(row)
   next if repo_name.empty? || repo_name == "repository_url"
-  puts client.repository(repo_name)
+  # You'll want to have this collect the data you're interested in from each repository.
+  puts client.repository(repo_name).inspect
 end
